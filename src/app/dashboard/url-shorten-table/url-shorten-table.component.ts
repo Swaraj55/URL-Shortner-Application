3d33af1156adc890f42bf64326e9cea315bae012
Swaraj Kumar <leo.swaraj.5@gmail.com>
2022-05-08 23:45:36 +0530

    Update url-shorten-table.component.ts

--- src/app/dashboard/url-shorten-table/url-shorten-table.component.ts
+++ src/app/dashboard/url-shorten-table/url-shorten-table.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
+
 
 @Component({
   selector: 'app-url-shorten-table',
