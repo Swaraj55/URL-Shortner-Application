2d50e53e3f961dbc35b0c8896cdef25c64db54b5
Swaraj Kumar <leo.swaraj.5@gmail.com>
2022-06-26 23:58:11 +0530

    Started working onQR generator section

--- src/app/dashboard/qr-generator/qr-generator.module.ts
+++ src/app/dashboard/qr-generator/qr-generator.module.ts
@@ -4,10 +4,16 @@ import { CommonModule } from '@angular/common';
 import { QrGeneratorComponent } from './qr-generator.component';
 import { QrGeneratorService } from './qr-generator.service';
 
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
 @NgModule({
   declarations: [QrGeneratorComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatSelectModule,
+    MatFormFieldModule
   ],
   exports: [QrGeneratorComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
