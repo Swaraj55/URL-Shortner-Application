9f5e2dc699f2ace42fb8df6823b5f3370a96977a
Swaraj Kumar <leo.swaraj.5@gmail.com>
2025-04-13 17:15:44 +0530

    Landing page mostly done two section left

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -9,115 +9,12 @@ import { Router } from '@angular/router';
 })
 export class LandingPageComponent implements OnInit {
 
-  benefits_sections_contents = [
-    {
-      icons: './assets/group-6.png',
-      content: 'Short URL is easy and fast, enter the long link to get your shortened link.',
-      bgColor: '#3E4065'
-    },
-    {
-      icons: './assets/group-5.png',
-      content: 'Check the amount of clicks that your shortened url received.',
-      bgColor: '#33377A'
-    },
-    {
-      icons: './assets/group-4.png',
-      content: 'Use any link, no matter what size, ShortURL always shortens.',
-      bgColor: '#3E4065'
-    },
-    {
-      icons: './assets/group-2.png',
-      content: 'Short URL is easy and fast, enter the long link to get your shortened link.',
-      bgColor: '#33377A'
-    },
-    {
-      icons: './assets/group-6.png',
-      content: 'Short URL is easy and fast, enter the long link to get your shortened link.',
-      bgColor: '#3E4065'
-    },
-    {
-      icons: './assets/group-3.png',
-      content: 'Short URL is easy and fast, enter the long link to get your shortened link.',
-      bgColor: '#33377A'
-    }
-  ];
-
-  feature_sections_contents = [
-    {
-      icons: './assets/group-1.png',
-      title: 'URL Shortner',
-      sub_points: [
-        'URL shortening at scale',
-        'URL shortening at scale',
-        'URL shortening at scale'
-      ],
-      content: 'A comprehensive solution to help make every point of connection between your content and your audience more powerful.',
-    },
-    {
-      icons: './assets/group-1.png',
-      title: 'QR Codes',
-      sub_points: [
-        'Fully customizable QR Codes',
-        'Fully customizable QR Codes',
-        'Fully customizable QR Codes'
-      ],
-      content: 'QR Code solutions for every customer, business and brand experience',
-    },
-    {
-      icons: './assets/group-1.png',
-      title: 'Link in Bio',
-      sub_points: [
-        'Fully customizable QR Codes',
-        'Fully customizable QR Codes',
-        'Fully customizable QR Codes'
-      ],
-      content: 'Link-in-bio, Link Management, to help you curate, package and track your best links',
-    },
-  ];
-
-  achievements_card_info = [
-    {
-      count: '500K',
-      count_info: 'Global Payments'
-    },
-    {
-      count: '500K',
-      count_info: 'Global Payments'
-    },
-    {
-      count: '500K',
-      count_info: 'Global Payments'
-    },
-    {
-      count: '500K',
-      count_info: 'Global Payments'
-    }
-  ];
-
   testimonials = [
-    {
-      rating_image: 'assets/rating.svg',
-      comma_image: 'assets/comma.svg',
-      quote: 'This is an amazing product! I highly recommend it to everyone.',
-      name: 'Client Name 1',
-      position: 'Position 1, Company 1'
-    },
-    {
-      rating_image: 'assets/rating.svg',
-      comma_image: 'assets/comma.svg',
-      quote: 'This is an amazing product! I highly recommend it to everyone.',
-      name: 'Client Name 2',
-      position: 'Position 2, Company 2'
-    },
-    {
-      rating_image: 'assets/rating.svg',
-      comma_image: 'assets/comma.svg',
-      quote: 'This is an amazing product! I highly recommend it to everyone.',
-      name: 'Client Name 2',
-      position: 'Position 2, Company 2'
-    }
+    { name: 'Swaraj KC', text: 'Very satisfied with the services\namazing experience', rating: 5 },
+    { name: 'Swaraj KC', text: 'Very satisfied with the services\namazing experience', rating: 5 },
+    { name: 'Swaraj KC', text: 'Very satisfied with the services\namazing experience', rating: 5 }
   ];
-
+  
   constructor(
     private router: Router
   ) {
